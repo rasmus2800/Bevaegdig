bfa70385f0d242284d1d27bc816c86c84addeed4
rasmus2800 <44200083+rasmus2800@users.noreply.github.com>
2023-10-31 14:03:40 +0100

    import

---
+++ script.js
@@ -0,0 +1,58 @@
+// bemærk hvis man skal oprette flere canvas og køre ikke-global mode
+// https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace
+// https://www.youtube.com/watch?v=Su792jEauZg
+// derman kan man oprette instans som objekt, hvori sketchen kører
+// Og man kan oprette flere canvas med samme sketch eller
+// forskellige sketches i forskellige canvas
+// instans mode skal closure på variable - namespacing - vs global mode med globale variable
+
+// tilføj knapper, og tilføj eventlistener til knappen, funktionskald
+// 10 spring til siden hurtigt nok - gør baggrunden helt hvid...
+
+let y = 10; let r = 300; g = 180; b = 0;
+let rystet = 0;
+let flyttet = 0;
+
+function setup() {
+    canvas = createCanvas(300, 550, 'beholder');
+    textSize(24);
+    // giver canvas border på 2 pixel, 
+    // og sørger derefter for at kanten tælles med i width
+    canvas.elt.style.border = '5px solid black';
+    canvas.elt.style.boxSizing = 'border-box';
+    canvas.elt.style.borderRadius = '20px';
+
+    canvas.parent('#beholder');
+    // gør canvas-elementet responsivt til skærmbredden
+    canvas.elt.style.width = '100%';
+    canvas.elt.style.height = '100%';
+
+    //bemærk at noden skal pakkes ud via .elt
+    const parentDiv = select('#beholder').elt;
+    const p = select('#test1').elt;
+    // indsæt canvas i ny position i rækkefølgen af elementer i div'en beholder
+    parentDiv.insertBefore(canvas.elt, p);
+}
+
+function draw() {
+    background(r, g, b);
+    strokeWeight(10);
+    ellipse(width / 2, y, 50);
+    y++;
+    if (accelerationX > 70) {
+        r = random(0, 256);
+        g = random(0, 256);
+        b = random(0, 256);
+        y--;
+        if(rystet%2 == 0)
+        rystet++;
+    }
+    if (y<height/2+25,y>height/2-25){
+        flyttet++;
+    }
+    if(flyttet>rystet){
+        rystet=flyttet;
+    }
+text('highscore:' + str(rystet), 25, height-50);
+text('point:' + str(flyttet),25, height-100);
+}
