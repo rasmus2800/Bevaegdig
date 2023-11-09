cd917179ed7cf39a660b091fef2d863d9a715f4b
rasmus2800 <44200083+rasmus2800@users.noreply.github.com>
2023-11-09 01:37:57 +0100

    fixd 2

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// bemærk hvis man skal oprette flere canvas og køre ikke-global mode
-// https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace
-// https://www.youtube.com/watch?v=Su792jEauZg
-// derman kan man oprette instans som objekt, hvori sketchen kører
-// Og man kan oprette flere canvas med samme sketch eller
-// forskellige sketches i forskellige canvas
-// instans mode skal closure på variable - namespacing - vs global mode med globale variable
-
-// tilføj knapper, og tilføj eventlistener til knappen, funktionskald
-// 10 spring til siden hurtigt nok - gør baggrunden helt hvid...
 
 let y = 10; let r = 250; g = 200; b = 0;
 let highscore = 0;
@@ -17,21 +7,16 @@ let speed = 1;
 function setup() {
     canvas = createCanvas(300, 550, 'beholder');
     textSize(24);
-    // giver canvas border på 2 pixel, 
-    // og sørger derefter for at kanten tælles med i width
     canvas.elt.style.border = '5px solid black';
     canvas.elt.style.boxSizing = 'border-box';
     canvas.elt.style.borderRadius = '10px';
 
     canvas.parent('#beholder');
-    // gør canvas-elementet responsivt til skærmbredden
     canvas.elt.style.width = '100%';
     canvas.elt.style.height = '100%';
 
-    //bemærk at noden skal pakkes ud via .elt
     const parentDiv = select('#beholder').elt;
     const p = select('#test1').elt;
-    // indsæt canvas i ny position i rækkefølgen af elementer i div'en beholder
     parentDiv.insertBefore(canvas.elt, p);
 }
 
@@ -60,7 +45,7 @@ function draw() {
         y = y - 1.5;
     }
     else
-        y=y+speed;
+        y = y + speed;
 
     if(y >= height || y <= 0){
         y = y - speed;
@@ -71,7 +56,7 @@ function draw() {
         point++;
     }
     if(point>highscore){
-        highscore=point;
+        highscore = point;
     }
 
 fill(0);
